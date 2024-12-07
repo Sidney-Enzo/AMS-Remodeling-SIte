1867694bb0b8fcf926c39719484d882a5c76b472
Foxy <sidneyenzorodriguesdasilva@gmail.com>
2024-12-07 11:28:03 +0000

    remake

--- scripts/about.js
+++ scripts/about.js
@@ -1,15 +1,14 @@
 let buttons = document.getElementsByClassName('toggleButton');
 let texts = document.getElementsByClassName('extraText');
 
-for (let i = 0; i < buttons.length; i++) 
-{
+for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', () => {
-        if (texts[i].style.display == 'none') {
-            texts[i].style.display = 'block';
-            buttons[i].textContent = '\u25B2';
+        if (texts[i].classList.contains('active')) {
+            texts[i].classList.remove('active');
+            buttons[i].textContent = '\u25BC'; // Ícone de seta para baixo
         } else {
-            texts[i].style.display = 'none';
-            buttons[i].textContent = '\u25BC';
+            texts[i].classList.add('active');
+            buttons[i].textContent = '\u25B2'; // Ícone de seta para cima
         }
-    })
+    });
 }
